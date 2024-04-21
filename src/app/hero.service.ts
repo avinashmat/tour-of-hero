af8332b325e2688fa031597b2a2128db9cfc6f51
Avinash Goud Matta <amatta1@lamar.edu>
2024-04-21 13:04:57 -0500

    Added feature to add and delete a hero from the list

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,6 +10,9 @@ import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class HeroService {
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
   private heroesUrl = 'api/heroes'; 
   constructor(private messageService: MessageService, private http: HttpClient) {
     
@@ -28,7 +31,28 @@ export class HeroService {
       catchError(this.handleError<Hero>(`getHero id=${id}`))
     );
   }
-  
+  updateHero(hero: Hero): Observable<any> {
+
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+addHero(hero: Hero): Observable<Hero> {
+  return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+    tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+    catchError(this.handleError<Hero>('addHero'))
+  );
+}
+deleteHero(id: number): Observable<Hero> {
+  const url = `${this.heroesUrl}/${id}`;
+
+  return this.http.delete<Hero>(url, this.httpOptions).pipe(
+    tap(_ => this.log(`deleted hero id=${id}`)),
+    catchError(this.handleError<Hero>('deleteHero'))
+  );
+}
+
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
